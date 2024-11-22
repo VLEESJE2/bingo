df6c895320ec6a8c7e2a49e2d669953669282581
VLEESJE2 <123599096+VLEESJE2@users.noreply.github.com>
2024-11-22 16:29:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const prompts = [
     "ik schaam mij", "het spijt me", "ik hou een notitie bij", "ik wou jullie er niet in mee trekken", "Ik ben sober",
     "ik had u nooit mee in de auto mogen nemen", "ik zoek hulp", "was nooit men bedoeling", "kheb echt niks gepakt", "da jullie na mijn thuis moesten komen",
     "ik laat me opneme als dees nie werkt", "ik ben aant afbouwen", "jullie zijn de enige die ik heb", "ik doe dit voor jullie", "ik heb zoveel aan jullie",
-    "de dokter heeft dit gezegd", "geef me nog een kans", "da was nooit men bedoeling", "kwil nie da jullie mij onder invloed zien", "ik hou heel veel van jullie",
+    "de dokter heeft dit gezegd", "geef me nog een kans", "ik was altijd sober op dussart", "kwil nie da jullie mij onder invloed zien", "ik hou heel veel van jullie",
     "jullie hebbe zoveel voor mij gedaan", "Ik doe da nie alleen voor mij maar vrienden en familie", "gaat nooit meer gebeuren", "when?"
 ];
 
